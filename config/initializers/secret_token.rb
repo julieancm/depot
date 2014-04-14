07158c074dae50a8ff78d1b1b05cfea2613b98ea
julie <julieancm@gmail.com>
2014-04-14 15:14:27 +0900

    First depot commit from Aptana

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Depot::Application.config.secret_key_base = '76e8f7d145c9bb0afc2c8ef7ade73d69f9816b41b88ec319d2860526bdd2d3b03e0c315fa11c7b653ecd1bb5112ff4df3c9e5839b6c3a70d4681c855512e8317'
